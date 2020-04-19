5d35ab95adfd0374f16e6f7a4aa2eff80a275fbc
kristiyan8807 <kristiyan8807@gmail.com>
2020-04-19 19:42:14 +0300

    aspdotnetcoretodo

--- AspNetCoreTodo/Data/ApplicationDbContext.cs
+++ AspNetCoreTodo/Data/ApplicationDbContext.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<TodoItem> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // ...
         }
     }
 }
---
+++ AspNetCoreTodo/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace AspNetCoreTodo.Models
+{
+
+    public class ApplicationUser : IdentityUser
+    {
+
+
+    }
+
+}
